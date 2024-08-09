de45f590921bcbd87e9811d80bd619c69ba68ea3
itserror808 <meftahahmedreda02@gmail.com>
2024-08-09 04:00:20 +0100

    UrbanVoyageBackend

---
+++ src/main/java/com/example/urbanvoyagebackend/UrbanVoyageBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.urbanvoyagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+UrbanVoyageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrbanVoyageBackendApplication.class, args);
+    }
+
+}
