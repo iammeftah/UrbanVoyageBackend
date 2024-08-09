535c28c7637c464cf33440644881abaf749ad464
itserror808 <meftahahmedreda02@gmail.com>
2024-08-09 04:14:07 +0100

    UrbanVoyageBackend

--- src/main/java/com/example/urbanvoyagebackend/UrbanVoyageBackendApplication.java
+++ src/main/java/com/example/urbanvoyagebackend/UrbanVoyageBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.urbanvoyagebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:.env")
 public class
 UrbanVoyageBackendApplication {
 
